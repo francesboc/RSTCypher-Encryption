3c0f7311ca77f1d1d790ad98b9cd9139645cc4cd
francesboc <58743819+francesboc@users.noreply.github.com>
2021-07-17 18:38:44 +0200

    finished test init table

--- db/init_table.sv
+++ db/init_table.sv
@@ -1,16 +1,8 @@
-/*
-Notes:
- 
- 1) How to detect repeated char?
-    -   first idea: feedback wire from output reg 'sub_char' to the input 'previous_char' and compare with key_char
-        that is initialized with NULL char (8'h00) (maybe previous_char not needed)
-*/
-
 module init_table (
   input             clk,
   input             rst_n,
   input      [95:0] key_char,
-  output reg [7:0]  sub_char[7][7],
+  output reg [6:0][6:0][7:0]  sub_char,
   output reg           err_repeated_char, // flag to detect repeated characters
   output reg           err_invalid_key_char // flag to detect invalid characters   
 );
@@ -30,68 +22,39 @@ module init_table (
 
   reg key_char_is_letter; 
   reg key_char_is_digit; 
-  reg bool_1; 
-  reg bool_2;
 
-  reg [7:0] rot_table[7][7];
-  reg [7:0] nul_table[7][7];
+  reg [6:0][6:0][7:0] rot_table = {49{NUL_CHAR}}; //initialization with null characters
+  reg [6:0][6:0][7:0] nul_table = {49{NUL_CHAR}};
 
   integer word_counter = 0;
   integer digit_counter = 0;
-  /*genvar i,j;
-  
-  // ---------------------------------------------------------------------------
-  // Logic Design
-  // ---------------------------------------------------------------------------
-  generate
-	  for(i=1; i < 12; i=i+1) begin
-		if(key_char[i]===key_char[i-1])
-			assign err_repeated_char = 1'b1;
-		assign key_char_is_letter = ((key_char[i] >= UPPERCASE_A_CHAR) && (key_char[i] <= UPPERCASE_Z_CHAR) || 
-								   (key_char[i] >= LOWERCASE_A_CHAR) && (key_char[i] <= LOWERCASE_Z_CHAR));
-		assign key_char_is_digit = (key_char[i] >= DIGIT_0_CHAR) && (key_char[i] <= DIGIT_9_CHAR);
-		assign err_invalid_key_char = !(key_char_is_letter || key_char_is_digit);
-	  end
-  endgenerate*/
-
-  reg k,l;
 
   always @(key_char) begin
     
-    bool_1 = 1'b0; 
-    bool_1 = 1'b0; 
-    for(int i=1; i < 12; i=i+1) begin 
-      if(key_char[i]===key_char[i-1]) 
-        bool_1 = 1'b1; 
-      key_char_is_letter = ((key_char[i] >= UPPERCASE_A_CHAR) && (key_char[i] <= UPPERCASE_Z_CHAR) ||  
-                              (key_char[i] >= LOWERCASE_A_CHAR) && (key_char[i] <= LOWERCASE_Z_CHAR)); 
-      key_char_is_digit = (key_char[i] >= DIGIT_0_CHAR) && (key_char[i] <= DIGIT_9_CHAR); 
-      if(!(key_char_is_letter || key_char_is_digit)) 
-        bool_2 = 1'b1; 
-    end 
- 
-    if(bool_1) 
-      err_repeated_char = 1'b1; 
-    if(bool_2) 
-      err_invalid_key_char = 1'b1;
-	
-    /*
-    95:88   11
-    87:80   10
-    79:72   9
-    71:64   8
-    63:56   7
-    55:48   6
-    47:40   5
-    39:32   4
-    31:24   3
-    23:16   2
-    15:8    1
-    7:0     0
+    //check presence of repeated or invalid characters in the key
+    // (i*8)+7:i*8 = i*8 +: 8 --> needed to indexing regs
+    err_repeated_char = 0;
+    err_invalid_key_char = 0;
+    for(int i=0; i<12; i=i+1) begin
+      key_char_is_letter = ((key_char[i*8 +: 8] >= UPPERCASE_A_CHAR) && (key_char[i*8 +: 8] <= UPPERCASE_Z_CHAR) ||  
+                            (key_char[i*8 +: 8] >= LOWERCASE_A_CHAR) && (key_char[i*8 +: 8] <= LOWERCASE_Z_CHAR));
+      key_char_is_digit = (key_char[i*8 +: 8] >= DIGIT_0_CHAR) && (key_char[i*8 +: 8] <= DIGIT_9_CHAR);
+      err_invalid_key_char = err_invalid_key_char || !(key_char_is_letter || key_char_is_digit);    
+      for(int j = 0; j< 12; j=j+1) begin
+        if(i!=j)
+          err_repeated_char = err_repeated_char || (key_char[i*8 +: 8] == key_char[j*8 +: 8]);
+      end
+      if(err_repeated_char || err_invalid_key_char) break;
+    end
+
+    /* 11       10      9       8
+       95:88    87:80   79:72   71:64
+       7        6       5       4
+       63:56    55:48   47:40   39:32
+       3        2       1       0
+       31:24    23:16   15:8    7:0
     */
     rot_table[0][0] = NUL_CHAR;
-	
-	
     if(!(err_repeated_char || err_invalid_key_char))
       //rows
       rot_table[1][0] = key_char[7:0];   // s[0] = k[0]
@@ -108,27 +71,17 @@ module init_table (
       rot_table[0][5] = key_char[47:40]; // s[5] = k[5]
       rot_table[0][6] = key_char[63:56]; // s[7] = k[7]
     
-	
-	
-	for (int k = 1; k<7; k=k+1) begin
-	  for (int l = 1; l<7; l=l+1) begin
-		if(k<=5 && l<= 2) begin
-		  rot_table[k][l] = LOWERCASE_A_CHAR + word_counter;
-		  word_counter = word_counter + 1;
-		end else begin
-		  rot_table[k][l] = DIGIT_0_CHAR + digit_counter;
-		  digit_counter = digit_counter + 1;
-		end
-	  end
-	end  
-	
-	for (int k = 0; k<7; k=k+1) begin
-	  for (int l = 0; l<7; l=l+1) begin
-		nul_table[k][l] = NUL_CHAR;
-	
-	  end
-	end
-	
+	  for (int k = 1; k<7; k=k+1) begin
+      for (int l = 1; l<7; l=l+1) begin
+        if( (k>4 && l>2) || k>5 ) begin
+        rot_table[k][l] = DIGIT_0_CHAR + digit_counter;
+        digit_counter = digit_counter + 1;
+        end else begin
+          rot_table[k][l] = LOWERCASE_A_CHAR + word_counter;
+          word_counter = word_counter + 1;
+        end
+      end
+    end  
   end
 
   // Table initialization
