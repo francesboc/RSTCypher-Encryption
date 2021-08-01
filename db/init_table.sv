8e6330108c8ca354fcb6ece0e726e5115c8f71de
Francesco <francesboc26@gmail.com>
2021-08-01 16:13:56 +0200

    refactoring module, fix sub and rotation, implemented tbs

--- db/init_table.sv
+++ db/init_table.sv
@@ -2,22 +2,20 @@ module init_table (
   input             clk,
   input             rst_n,
   input      [95:0] key_char,
-  input      [7:0] ptx_char,
-  input            ptxt_valid,
-  //output reg [6:0][6:0][7:0]  sub_char, //non restituisce più una tabella ma 2 caratteri di sostituzione
-  output reg [15:0] sub_str, //restituiamo una stringa di due caratteri 
-  //output reg           err_repeated_char, // flag to detect repeated characters
+  input      [7:0]  ptxt_char,
+  input             ptxt_valid,
+  output reg [15:0] ctxt_str, // two characters ciphertext output
   output reg        err_invalid_key, // flag to detect invalid characters
-  output reg        err_invalid_ptx_char, // flag to detect invalid plaintext char
-  output reg           ctxt_ready
+  output reg        err_invalid_ptxt, // flag to detect invalid plaintext char
+  output reg        err_key_not_installed, // flag to detect invalid plaintext char
+  output reg        ctxt_ready
 );
 
   // ---------------------------------------------------------------------------
   // Variables
   // ---------------------------------------------------------------------------
-
+  
   localparam NUL_CHAR = 8'h00;
-
   localparam UPPERCASE_A_CHAR = 8'h41;
   localparam UPPERCASE_Z_CHAR = 8'h5A;
   localparam LOWERCASE_A_CHAR = 8'h61;
@@ -25,26 +23,28 @@ module init_table (
   localparam DIGIT_0_CHAR     = 8'h30;
   localparam DIGIT_9_CHAR     = 8'h39;
 
-  reg err_repeated_char; // flag to detect repeated characters
-  reg err_invalid_key_char; // flag to detect invalid characters
-
-  reg char_is_letter; 
+  reg err_repeated_char = 0; // flag to detect repeated characters
+  reg err_invalid_key_char = 0; // flag to detect invalid characters
+  reg err_invalid_ptxt_char = 0;
+  reg char_is_letter;
   reg char_is_digit;
-  reg is_table_initialized = 1'b0; //initialization flag
+  reg is_table_initialized = 0; //initialization flag
   reg [7:0] temp_row;
   reg [7:0] temp_line;
 
   reg [6:0][6:0][7:0] rot_table = {49{NUL_CHAR}}; //initialization with null characters
-  //reg [6:0][6:0][7:0] nul_table = {49{NUL_CHAR}}; non più necessario
-  reg [15:0] nul_str = {2{NUL_CHAR}}; //2 caratteri nulli
-  reg [15:0] ctx_char;
+  reg [15:0] sub_str = {2{NUL_CHAR}};;
 
-  integer word_counter = 0;
-  integer digit_counter = 0;
+  integer word_counter;
+  integer digit_counter;
 
+  // ---------------------------------------------------------------------------
+  // Logic Design
+  // ---------------------------------------------------------------------------
+
+  //table initialization
   always @(key_char) begin
     if (!is_table_initialized) begin
-      err_invalid_key = 0;
       //check presence of repeated or invalid characters in the key
       // (i*8)+7:i*8 = i*8 +: 8 --> needed to indexing regs
       err_repeated_char = 0;
@@ -58,98 +58,129 @@ module init_table (
           if(i!=j)
             err_repeated_char = err_repeated_char || (key_char[i*8 +: 8] == key_char[j*8 +: 8]);
         end
-        if(err_repeated_char || err_invalid_key_char) begin
-          err_invalid_key = 1;
+        if(err_repeated_char || err_invalid_key_char)
           break;
-        end 
       end
-
-      /* 11       10      9       8
+      /*11(a)    10(b)   9(c)    8(d)
         95:88    87:80   79:72   71:64
-        7        6       5       4
+        7(e)     6(f)    5(g)    4(h)
         63:56    55:48   47:40   39:32
-        3        2       1       0
+        3(i)     2(j)    1(k)    0(l)
         31:24    23:16   15:8    7:0
       */
-      
-      rot_table[0][0] = NUL_CHAR;
+      //rot_table[0][0] = NUL_CHAR;
       if(!(err_repeated_char || err_invalid_key_char)) begin
-        is_table_initialized=1'b1;
+        is_table_initialized=1;
         //rows
-        rot_table[1][0] = key_char[7:0];   // s[0] = k[0]
-        rot_table[2][0] = key_char[87:80]; // s[10] = k[10]
-        rot_table[3][0] = key_char[23:16]; // s[2] = k[2]
-        rot_table[4][0] = key_char[71:64]; // s[8] = k[8]
-        rot_table[5][0] = key_char[39:32]; // s[4] = k[4]
-        rot_table[6][0] = key_char[55:48]; // s[6] = k[6]
+        rot_table[1][0] = key_char[95:88];   // s[0] = k[0] = a
+        rot_table[2][0] = key_char[15:8]; // s[10] = k[10] = k
+        rot_table[3][0] = key_char[79:72]; // s[2] = k[2] = c
+        rot_table[4][0] = key_char[31:24]; // s[8] = k[8] = i
+        rot_table[5][0] = key_char[63:56]; // s[4] = k[4] = e
+        rot_table[6][0] = key_char[47:40]; // s[6] = k[6] = g
         //columns
-        rot_table[0][1] = key_char[15:8];  // s[1] = k[1]
-        rot_table[0][2] = key_char[95:88]; // s[11] = k[11]
-        rot_table[0][3] = key_char[31:24]; // s[3] = k[3] 
-        rot_table[0][4] = key_char[79:72]; // s[9] = k[9]
-        rot_table[0][5] = key_char[47:40]; // s[5] = k[5]
-        rot_table[0][6] = key_char[63:56]; // s[7] = k[7]
-      end
-      for (int k = 1; k<7; k=k+1) begin
-        for (int l = 1; l<7; l=l+1) begin
-          if( (k>4 && l>2) || k>5 ) begin
-          rot_table[k][l] = DIGIT_0_CHAR + digit_counter;
-          digit_counter = digit_counter + 1;
-          end else begin
-            rot_table[k][l] = LOWERCASE_A_CHAR + word_counter;
-            word_counter = word_counter + 1;
-          end
-        end
-      end
-    end
-  end
+        rot_table[0][1] = key_char[87:80];  // s[1] = k[1] = b
+        rot_table[0][2] = key_char[7:0]; // s[11] = k[11] = l
+        rot_table[0][3] = key_char[71:64]; // s[3] = k[3] = d
+        rot_table[0][4] = key_char[23:16]; // s[9] = k[9] = j
+        rot_table[0][5] = key_char[55:48]; // s[5] = k[5] = f
+        rot_table[0][6] = key_char[39:32]; // s[7] = k[7] = h
 
-  always @(*) begin
-    if(is_table_initialized && ptxt_valid)begin
-      err_invalid_ptx_char = 0;
-      char_is_letter = ((ptx_char >= UPPERCASE_A_CHAR) && (ptx_char <= UPPERCASE_Z_CHAR) ||  
-                                (ptx_char >= LOWERCASE_A_CHAR) && (ptx_char <= LOWERCASE_Z_CHAR));
-      char_is_digit = (ptx_char >= DIGIT_0_CHAR) && (ptx_char <= DIGIT_9_CHAR);
-      err_invalid_ptx_char = !(char_is_letter || char_is_digit);
-
-      if(!err_invalid_ptx_char)begin
-        //Substituition
-        for (int r=1; r<7; r=r+1 ) begin
-          for (int c=1; c <7; c=c+1) begin
-            if( rot_table[r][c] == ptx_char ) begin
-              ctx_char[15:8] = rot_table[r][0];
-              ctx_char[7:0] = rot_table[0][c];
+        digit_counter = 0;
+        word_counter = 0;
+        //initialization with letters and digits
+        for (int k = 1; k<7; k=k+1) begin
+          for (int l = 1; l<7; l=l+1) begin
+            if( (k>4 && l>2) || k>5 ) begin
+            rot_table[k][l] = DIGIT_0_CHAR + digit_counter;
+            digit_counter = digit_counter + 1;
+            end else begin
+              rot_table[k][l] = LOWERCASE_A_CHAR + word_counter;
+              word_counter = word_counter + 1;
             end
           end
         end
-        //Ratation
-        temp_row = rot_table[0][7];
-        temp_line = rot_table[7][0];
-        for (int i=7; i>=2; i=i-1 ) begin 
-          rot_table[0][i] <= rot_table[0][i-1];
-          rot_table[i][0] <= rot_table[i-1][0];
-        end
-        rot_table[0][1] <= temp_row;
-        rot_table[1][0] <= temp_line;
       end
 
     end
-    //ROT operations
   end
 
-  // output di due caratteri alla volta
-  // vanno aggiunti i controlli...
+  // substitution and rotation
+  always @(*) begin
+    ctxt_ready = 0;
+    if(is_table_initialized && ptxt_valid)begin
+      //checks on plaintext
+      err_invalid_ptxt_char = 0;
+      char_is_letter = ((ptxt_char >= UPPERCASE_A_CHAR) && (ptxt_char <= UPPERCASE_Z_CHAR) ||  
+                        (ptxt_char >= LOWERCASE_A_CHAR) && (ptxt_char <= LOWERCASE_Z_CHAR));
+      char_is_digit = (ptxt_char >= DIGIT_0_CHAR) && (ptxt_char <= DIGIT_9_CHAR);
+      err_invalid_ptxt_char = !(char_is_letter || char_is_digit);
+
+      if(!err_invalid_ptxt_char)begin
+        //Substituition
+        for (int r=1; r<7; r=r+1 ) begin
+          for (int c=1; c <7; c=c+1) begin
+            if( rot_table[r][c] == ptxt_char || rot_table[r][c] == (ptxt_char + 8'd32)) begin
+              sub_str[15:8] = rot_table[r][0];
+              sub_str[7:0] = rot_table[0][c];
+            end
+          end
+        end
+        //Rotation
+        temp_row = rot_table[0][6];
+        temp_line = rot_table[6][0];
+        for (int i=6; i>=2; i=i-1 ) begin 
+          rot_table[0][i] = rot_table[0][i-1];
+          rot_table[i][0] = rot_table[i-1][0];
+        end
+        rot_table[0][1] = temp_row;
+        rot_table[1][0] = temp_line;
+      end
+
+    end
+  end
+
+  // Output string
   always @(posedge clk or negedge rst_n) begin
-    if(!rst_n)
-      sub_str <= nul_str;
-    else if(err_invalid_key || err_invalid_ptx_char) begin
-      sub_str <= nul_str;
+    if(!rst_n) begin
+      err_invalid_key <= 0;
+      err_invalid_ptxt <=0;
       ctxt_ready <= 0;
+      err_key_not_installed <= 0;
+      is_table_initialized = 0;
+      ctxt_str <= {2{NUL_CHAR}};
+    end else if(!is_table_initialized) begin
+      if(err_repeated_char || err_invalid_key_char) begin
+        err_invalid_key <= 1;
+        err_key_not_installed <= 0;
+      end else begin
+        err_invalid_key <= 0;
+        err_key_not_installed <= 1;
+      end
+      err_invalid_ptxt <= 0;
+      ctxt_ready <= 0;
+      ctxt_str <= {2{NUL_CHAR}};
+    // invalid plaintext character
+    end else if(err_invalid_ptxt_char) begin
+      err_invalid_key <= 0;
+      err_invalid_ptxt <= 1;
+      err_key_not_installed <= 0;
+      ctxt_ready <= 0;
+      ctxt_str <= {2{NUL_CHAR}};
+    //check this
+    end else if(!ptxt_valid) begin
+      err_invalid_key <= 0;
+      err_invalid_ptxt <= 0;
+      err_key_not_installed <= 0;
+      ctxt_ready <= 0;
+      ctxt_str <= {2{NUL_CHAR}};
     end else begin
-      sub_str <= ctx_char;
+      err_invalid_key <= 0;
+      err_invalid_ptxt <= 0;
+      err_key_not_installed <= 0;
       ctxt_ready <= 1;
-    end     
-       
+      ctxt_str <= sub_str;
+    end
   end
 
 endmodule
--- tb/init_table_tb.sv
+++ tb/init_table_tb.sv
@@ -15,20 +15,30 @@ module init_table_tb;
   end
   
   reg   [95:0]            key_char;
-  wire  [6:0][6:0][7:0]   sub_char;
+  reg   [7:0]             ptxt_char;
+  reg                     ptxt_valid;
+  reg   [15:0]           ctxt_char;
+  reg                    ctxt_ready;
+  reg                     err_invalid_key;
+  reg                     err_key_not_installed;
+  reg                     err_invalid_ptx_char;
 
   init_table init_table (
      .clk                       (clk)
     ,.rst_n                     (rst_n)
     ,.key_char                  (key_char)
-    ,.sub_char                  (sub_char)
-    ,.err_repeated_char         (/* Unconnected */)
-    ,.err_invalid_key_char      (/* Unconnected */)     
+    ,.ptxt_char                 (ptxt_char)
+    ,.ptxt_valid                (ptxt_valid)
+    ,.ctxt_str                  (ctxt_char)
+    ,.err_invalid_key           (err_invalid_key)
+    ,.err_invalid_ptxt          (err_invalid_ptx_char)
+    ,.err_key_not_installed     (err_key_not_installed)
+    ,.ctxt_ready                (ctxt_ready)
   );
 
-  reg [6:0][6:0][7:0] EXPECTED_GEN;
-  reg [6:0][6:0][7:0] EXPECTED_CHECK;
-  reg [6:0][6:0][7:0] EXPECTED_QUEUE[$];
+  reg [15:0] EXPECTED_GEN;
+  reg [15:0] EXPECTED_CHECK;
+  reg [15:0] EXPECTED_QUEUE[$];
   
   localparam NUL_CHAR = 8'h00;
   
@@ -39,52 +49,97 @@ module init_table_tb;
   localparam DIGIT_0_CHAR     = 8'h30;
   localparam DIGIT_9_CHAR     = 8'h39;
 
+  /*
+  reg char_is_letter = 0; 
+  reg char_is_digit = 0;
+  reg [6:0][6:0][7:0] rot_table = {49{NUL_CHAR}};
+  reg [6:0][6:0][7:0] aux_table = {49{NUL_CHAR}};
+  reg is_table_initialized = 0;
   
-  reg key_char_is_letter; 
-  reg key_char_is_digit;
-  reg [6:0][6:0][7:0] nul_table = {49{NUL_CHAR}};
-  
-  reg err_repeated_char;
-  reg err_invalid_key_char;
+  reg err_repeated_char = 0;
 
   integer word_counter = 0;
   integer digit_counter = 0;
+  reg temp_row;
+  reg temp_line;
 
-  task expected_matrix (
+  task expected_ctxt(
+    output [15:0] exp_char
+  );
+
+    if(is_table_initialized && ptxt_valid)begin
+      err_invalid_ptx_char = 0;
+      char_is_letter = ((ptxt_char >= UPPERCASE_A_CHAR) && (ptxt_char <= UPPERCASE_Z_CHAR) ||  
+                                (ptxt_char >= LOWERCASE_A_CHAR) && (ptxt_char <= LOWERCASE_Z_CHAR));
+      char_is_digit = (ptxt_char >= DIGIT_0_CHAR) && (ptxt_char <= DIGIT_9_CHAR);
+      err_invalid_ptx_char = !(char_is_letter || char_is_digit);
+
+      if(!err_invalid_ptx_char)begin
+        // substitution
+        for (int r=1; r<7; r=r+1 ) begin
+          for (int c=1; c <7; c=c+1) begin
+            if( rot_table[r][c] == ptxt_char ) begin
+              exp_char[15:8]  = rot_table[r][0];
+              exp_char[7:0]   = rot_table[0][c];
+            end
+          end
+        end
+        // rotation
+        temp_row = rot_table[0][7];
+        temp_line = rot_table[7][0];
+        for (int i=7; i>=2; i=i-1 ) begin 
+          rot_table[0][i] <= rot_table[0][i-1];
+          rot_table[i][0] <= rot_table[i-1][0];
+        end
+        rot_table[0][1] <= temp_row;
+        rot_table[1][0] <= temp_line;
+      end else begin
+        exp_char[15:8]  = NUL_CHAR;
+        exp_char[7:0]   = NUL_CHAR;
+      end
+    end else begin
+      exp_char[15:8]  = NUL_CHAR;
+      exp_char[7:0]   = NUL_CHAR;
+    end
+  endtask
+  
+  
+  task initialize_aux_table (
      output [6:0][6:0][7:0] rot_table
   );
     
     //check presence of repeated or invalid characters in the key
     // (i*8)+7:i*8 = i*8 +: 8
     err_repeated_char = 0;
-    err_invalid_key_char = 0;
+    err_invalid_key = 0;
     for(int i=0; i<12; i=i+1) begin
-      key_char_is_letter = ((key_char[i*8 +: 8] >= UPPERCASE_A_CHAR) && (key_char[i*8 +: 8] <= UPPERCASE_Z_CHAR) ||  
+      char_is_letter = ((key_char[i*8 +: 8] >= UPPERCASE_A_CHAR) && (key_char[i*8 +: 8] <= UPPERCASE_Z_CHAR) ||  
                               (key_char[i*8 +: 8] >= LOWERCASE_A_CHAR) && (key_char[i*8 +: 8] <= LOWERCASE_Z_CHAR));
-      key_char_is_digit = (key_char[i*8 +: 8] >= DIGIT_0_CHAR) && (key_char[i*8 +: 8] <= DIGIT_9_CHAR);
-      err_invalid_key_char = err_invalid_key_char || !(key_char_is_letter || key_char_is_digit); 
+      char_is_digit = (key_char[i*8 +: 8] >= DIGIT_0_CHAR) && (key_char[i*8 +: 8] <= DIGIT_9_CHAR);
+      err_invalid_key = err_invalid_key || !(char_is_letter || char_is_digit); 
       for(int j = 0; j< 12; j=j+1) begin
         if(i!=j) err_repeated_char = err_repeated_char || (key_char[i*8 +: 8] == key_char[j*8 +: 8]);
       end   
-      if(err_repeated_char || err_invalid_key_char) break;
+      if(err_repeated_char || err_invalid_key) break;
     end
 
     rot_table[0][0] = NUL_CHAR;
-    if(!(err_repeated_char || err_invalid_key_char)) begin
+    if(!(err_repeated_char || err_invalid_key)) begin
+      is_table_initialized=1'b1;
       //rows
-      rot_table[1][0] = key_char[7:0];   // s[0] = k[0]
-      rot_table[2][0] = key_char[87:80]; // s[10] = k[10]
-      rot_table[3][0] = key_char[23:16]; // s[2] = k[2]
-      rot_table[4][0] = key_char[71:64]; // s[8] = k[8]
-      rot_table[5][0] = key_char[39:32]; // s[4] = k[4]
-      rot_table[6][0] = key_char[55:48]; // s[6] = k[6]
+      rot_table[1][0] = key_char[95:88];   // s[0] = k[0] = a
+      rot_table[2][0] = key_char[15:8]; // s[10] = k[10] = k
+      rot_table[3][0] = key_char[79:72]; // s[2] = k[2] = c
+      rot_table[4][0] = key_char[31:24]; // s[8] = k[8] = i
+      rot_table[5][0] = key_char[63:56]; // s[4] = k[4] = e
+      rot_table[6][0] = key_char[47:40]; // s[6] = k[6] = g
       //columns
-      rot_table[0][1] = key_char[15:8];  // s[1] = k[1]
-      rot_table[0][2] = key_char[95:88]; // s[11] = k[11]
-      rot_table[0][3] = key_char[31:24]; // s[3] = k[3] 
-      rot_table[0][4] = key_char[79:72]; // s[9] = k[9]
-      rot_table[0][5] = key_char[47:40]; // s[5] = k[5]
-      rot_table[0][6] = key_char[63:56]; // s[7] = k[7]
+      rot_table[0][1] = key_char[87:80];  // s[1] = k[1] = b
+      rot_table[0][2] = key_char[7:0]; // s[11] = k[11] = l
+      rot_table[0][3] = key_char[71:64]; // s[3] = k[3] = d
+      rot_table[0][4] = key_char[23:16]; // s[9] = k[9] = j
+      rot_table[0][5] = key_char[55:48]; // s[5] = k[5] = f
+      rot_table[0][6] = key_char[39:32]; // s[7] = k[7] = h
     end
     
     for (int k = 1; k<7; k=k+1) begin
@@ -99,39 +154,126 @@ module init_table_tb;
       end
     end  
   endtask
-  
+  */
   initial begin
-    @(reset_deassertion); // mi allineo a questo evento, ovvero lo aspetto
+    @(reset_deassertion);
+    @(posedge clk);
+    //is_table_initialized = 0;
     
-    @(posedge clk); // aspetto il fronte positivo
-    
+    begin: TEST_KEY_NOT_INSTALLED
+      $display("--> 1 %s %d %d %d %d",ctxt_char, ctxt_ready, err_invalid_key, err_invalid_ptx_char,err_key_not_installed);
+      ptxt_valid = 0;
+      @(posedge clk);
+      ptxt_valid = 1;
+      ptxt_char = "a";
+      $display("--> 2 %s %d %d %d %d",ctxt_char, ctxt_ready, err_invalid_key, err_invalid_ptx_char,err_key_not_installed);
+      @(posedge clk);
+      key_char = "abcdefghijkl";
+      ptxt_valid = 1;
+      ptxt_char = "b";
+      $display("--> 3 %s %d %d %d %d",ctxt_char, ctxt_ready, err_invalid_key, err_invalid_ptx_char,err_key_not_installed);
+      @(posedge clk);
+      ptxt_valid = 1;
+      ptxt_char = "a";
+      @(posedge clk);
+      $display("--> 4 %s %d %d %d %d",ctxt_char, ctxt_ready, err_invalid_key, err_invalid_ptx_char,err_key_not_installed);
+      @(posedge clk);
+      $display("--> 5 %s %d %d %d %d",ctxt_char, ctxt_ready, err_invalid_key, err_invalid_ptx_char,err_key_not_installed);
+    end: TEST_KEY_NOT_INSTALLED
+
+    begin: TEST_WRONG_KEY
+      rst_n = 0;
+      @(posedge clk);
+      rst_n = 1;
+      key_char = "abcdefghi?kl";
+      ptxt_valid = 1;
+      ptxt_char = "b";
+      $display("--> 1 %s %d %d %d %d",ctxt_char, ctxt_ready, err_invalid_key, err_invalid_ptx_char,err_key_not_installed);
+      @(posedge clk);
+      key_char = "abcdabcdabcd";
+      ptxt_valid = 1;
+      ptxt_char = "a";
+      $display("--> 2 %s %d %d %d %d",ctxt_char, ctxt_ready, err_invalid_key, err_invalid_ptx_char,err_key_not_installed);
+      @(posedge clk);
+      key_char = "abcdefghijkl";
+      ptxt_valid = 1;
+      ptxt_char = "b";
+      $display("--> 3 %s %d %d %d %d",ctxt_char, ctxt_ready, err_invalid_key, err_invalid_ptx_char,err_key_not_installed);
+      @(posedge clk);
+      key_char = "abcdefghijkl";
+      ptxt_valid = 1;
+      ptxt_char = "c";
+      $display("--> 4 %s %d %d %d %d",ctxt_char, ctxt_ready, err_invalid_key, err_invalid_ptx_char,err_key_not_installed);
+      @(posedge clk);
+      $display("--> 5 %s %d %d %d %d",ctxt_char, ctxt_ready, err_invalid_key, err_invalid_ptx_char,err_key_not_installed);
+      @(posedge clk);
+      $display("--> 6 %s %d %d %d %d",ctxt_char, ctxt_ready, err_invalid_key, err_invalid_ptx_char,err_key_not_installed);
+    end
+
+    /*
+    begin: TEST_INIT_TABLE
+      key_char = "abcdefghijkl";
+      initialize_aux_table(aux_table);
+      for(int i=1; i<7; i++)begin
+        $display("(%d,0) %c", i, aux_table[i][0]);
+      end
+      for(int j=1;j<7;j++)begin
+         $display("(0,%d) %c", j, aux_table[0][j]);
+      end
+    end: TEST_INIT_TABLE
+    */
+    /*
+    begin: TEST_SINGLE_SUB_ROT
+      $display("START TEST %s %d %d %d",ctxt_char, ctxt_ready, err_invalid_key, err_invalid_ptx_char);
+      key_char = "abcdefghijkl";
+      ptxt_valid = 0;
+      ptxt_char = NUL_CHAR;
+      @(posedge clk);
+      for(int i = 0; i < 26; i=i+2) begin
+          ptxt_char = "a" + i;
+          ptxt_valid = 1;
+          @(posedge clk);
+          $display("%s %d %d %d",ctxt_char, ctxt_ready, err_invalid_key, err_invalid_ptx_char);
+      end
+    end: TEST_SINGLE_SUB_ROT*/
+
+    /*
     fork
 
-      begin: STIMULI_1R
-        key_char = "abcdefghilmn";
-        @(posedge clk);
-        expected_matrix(EXPECTED_GEN);
-	      for(int i = 0; i < 7; i++) begin
-          for (int j = 0; j < 7; j++) begin
-             $display("EG: (%d,%d) %c ",i,j, EXPECTED_GEN[i][j]);
-          end
+      begin: TEST_WORK
+        key_char = "abcdefghijkl";
+        initialize_aux_table(rot_table);
+        for(int i =0; i<26; i++) begin
+          ptx_char = "a" + i;
+          ptxt_valid = 1;
+          @(posedge clk);
+          expected_ctxt(EXPECTED_GEN);
+          $display("--> %s", EXPECTED_GEN);
+          EXPECTED_QUEUE.push_back(EXPECTED_GEN);
         end
-        EXPECTED_QUEUE.push_back(EXPECTED_GEN);
-      end: STIMULI_1R
 
-      begin: CHECK_1R
-        @(posedge clk);
+        for(int i=0; i<10; i++) begin
+          ptx_char = "0" + i;
+          ptxt_valid = 1;
+          @(posedge clk);
+          expected_ctxt(EXPECTED_GEN);
+          EXPECTED_QUEUE.push_back(EXPECTED_GEN);
+        end
+      end: TEST_WORK
+
+      begin: CHECK_WORK
         @(posedge clk);
         EXPECTED_CHECK = EXPECTED_QUEUE.pop_front();
-        for(int i = 0; i < 7; i++) begin
-          for (int j = 0; j < 7; j++) begin
-            $display("%c %c %-5s", sub_char[i][j], EXPECTED_CHECK[i][j], EXPECTED_CHECK[i][j] === sub_char[i][j] ? "OK" : "ERROR");
-          end
-          //if(EXPECTED_CHECK != nul_table) $stop;
+        for(int i = 0; i < 36; i++) begin
+          @(posedge clk);
+          EXPECTED_CHECK = EXPECTED_QUEUE.pop_front();
+          $display("%s %s %-5s", ctxt_char, EXPECTED_CHECK, EXPECTED_CHECK === ctxt_char ? "OK" : "ERROR");
+          if(EXPECTED_CHECK != ctxt_char) $stop;
         end
-      end: CHECK_1R
+      end: CHECK_WORK
+      
     join
-
+    */
     $stop;
     
   end
